25d89181b53c98181533200d08b15202eefd5079
76500833 <145300587+76500833@users.noreply.github.com>
2024-01-26 18:34:44 -0500

    tables created

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Insert seeds into the `department` table
+INSERT INTO department (name)
+VALUES ('Sales'), ('Marketing'), ('Finance');
+
+-- Insert seeds into the `role` table
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Manager', 50000, 1), ('Marketing Coordinator', 40000, 2), ('Accountant', 45000, 3);
+
+-- Insert seeds into the `employees` table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Smith', 2, 1), ('Mike', 'Johnson', 3, 1);
+
+--? John is the manager of mike and jane
