8ddb2ba73924489ad9f9a72e363d8baaf2dbc11b
76500833 <145300587+76500833@users.noreply.github.com>
2024-01-26 22:11:43 -0500

    added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE role(
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -19,5 +20,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NULL
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
