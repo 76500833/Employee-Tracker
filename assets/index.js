1618c1bde9b0d634fafaca6626a55e754c153b40
76500833 <145300587+76500833@users.noreply.github.com>
2024-01-26 20:31:32 -0500

    can add roles

--- assets/index.js
+++ assets/index.js
@@ -8,13 +8,13 @@ const connection = mysql.createConnection({
     user: 'root',
     password: '9203',
     database: 'employees_db'
-  });
+});
 const questions = [
     {
         type: 'list',
         name: 'select',
         message: 'What would you like to do? ',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add and employee', 'update an employee(s) role'],
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add and employee', 'update an employee(s) role'],
     }
 ];
 
@@ -23,13 +23,13 @@ const promptUser = () => {
     inquirer.prompt(questions)
         .then((answers) => {
             if (answers.select === 'view all departments') {
-                connection.query('SELECT * FROM department', function(err, results) {
+                connection.query('SELECT * FROM department', function (err, results) {
                     if (err) throw err;
                     results.forEach(department => {
                         console.log(`ID: ${department.id}, Name: ${department.name}`);
                     });
                 });
-            } 
+            }
             if (answers.select === 'view all roles') {
                 connection.query('SELECT * FROM role', function (err, results) {
                     if (err) throw err;
@@ -37,7 +37,7 @@ const promptUser = () => {
                         console.log(`ID: ${role.id}, Title: ${role.title}, Salary: ${role.salary}, Department ID: ${role.department_id}`);
                     });
                 });
-            } 
+            }
             if (answers.select === 'view all employees') {
                 connection.query('SELECT * FROM employee', function (err, results) {
                     if (err) throw err;
@@ -46,8 +46,56 @@ const promptUser = () => {
                     });
                 });
             }
+            if (answers.select === 'add a department') {
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'departmentName',
+                    message: 'What is the name of the department you wish to add?',
+                })
+                    .then((answers) => {
+
+
+                        connection.query('INSERT INTO department SET ?', {
+                            name: answers.departmentName
+                        }, function (err, res) {
+                            if (err) throw err;
+                            console.log(`Succesfully added ${answers.departmentName} department!`);
+                        });
+                    })
+            }
+            //! Add a role
+            if (answers.select === 'add a role') {
+                inquirer.prompt([{
+                    type: 'input',
+                    name: 'roleName',
+                    message: 'What is the name of the role you wish to add?',
+                },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'What is the salary for this role?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'departmentId',
+                        message: 'What is the department ID for this role?',
+                    }]
+                )
+                    .then((answers) => {
+
+
+                        connection.query('INSERT INTO role SET ?', {
+                            title: answers.roleName,
+                            salary: answers.salary,
+                            department_id: answers.departmentId
+                        }, function (err, res) {
+                            if (err) throw err;
+                            console.log(`Succesfully added ${answers.roleName} role!`);
+                        });
+                    })
+            }
             // Handle other choices here...
-        });
+        })
 }
 
 function init() {
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,4 +10,4 @@ VALUES ('Sales Manager', 50000, 1), ('Marketing Coordinator', 40000, 2), ('Accou
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Smith', 2, 1), ('Mike', 'Johnson', 3, 1);
 
---? John is the manager of mike and jane
+-- John is the manager of mike and jane
